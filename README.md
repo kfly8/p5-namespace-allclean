836fa8effd7961ab15934948035337bb064472f3
kfly8 <kfly@cpan.org>
2021-02-23 23:36:12 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+namespace::allclean - It's new $module
+
+# SYNOPSIS
+
+    use namespace::allclean;
+
+# DESCRIPTION
+
+namespace::allclean is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly8@cpan.org>
---
+++ lib/namespace/allclean.pm
@@ -0,0 +1,39 @@
+package namespace::allclean;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+namespace::allclean - It's new $module
+
+=head1 SYNOPSIS
+
+    use namespace::allclean;
+
+=head1 DESCRIPTION
+
+namespace::allclean is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly8@cpan.orgE<gt>
+
+=cut
+
